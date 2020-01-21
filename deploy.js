c926466841e3b734a3fec568809a3a4e8b90b30b
Larry Cousino <lawrence.cousino@gmail.com>
2020-01-20 20:08:58 -0500

    Added automation to travis.yml 7

--- deploy.js
+++ deploy.js
@@ -87,7 +87,7 @@ function sshConnect() {
       // TODO: ADD YOUR IP ADDRESS BELOW (e.g. '12.34.5.67')
       host: '54.198.227.85',
       username: 'ubuntu',
-      privateKey: 'hs-key.pem'
+      privateKey: 'hs2-key.pem'
     })
     .then(function() {
       console.log('SSH Connection established.');
